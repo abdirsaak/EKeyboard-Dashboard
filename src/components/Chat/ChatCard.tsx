b7a48c653710eb235273914a80ea4be07d498d59
YOUSUF <yufis0844@gmail.com>
2024-02-21 14:36:59 +0300

    dashboard

--- src/components/Chat/ChatCard.tsx
+++ src/components/Chat/ChatCard.tsx
@@ -61,7 +61,7 @@ const ChatCard = () => {
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
       <h4 className="mb-6 px-7.5 text-xl font-semibold text-black dark:text-white">
-        Chats
+        Latest customers
       </h4>
 
       <div>
@@ -75,7 +75,7 @@ const ChatCard = () => {
               <img src={chat.avatar} alt="User" />
               <span
                 className="absolute right-0 bottom-0 h-3.5 w-3.5 rounded-full border-2 border-white"
-                style={{backgroundColor: chat.color}}
+                style={{ backgroundColor: chat.color }}
               ></span>
             </div>
 
@@ -84,21 +84,7 @@ const ChatCard = () => {
                 <h5 className="font-medium text-black dark:text-white">
                   {chat.name}
                 </h5>
-                <p>
-                  <span className="text-sm text-black dark:text-white">
-                    {chat.text}
-                  </span>
-                  <span className="text-xs"> . {chat.time} min</span>
-                </p>
               </div>
-              {chat.textCount !== 0 && (
-                <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary">
-                  <span className="text-sm font-medium text-white">
-                    {' '}
-                    {chat.textCount}
-                  </span>
-                </div>
-              )}
             </div>
           </Link>
         ))}
